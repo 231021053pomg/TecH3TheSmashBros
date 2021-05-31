8da69483dcab8f60d52b22ff0998e7803d4c290d
LionheartIV <54432930+LionheartIV@users.noreply.github.com>
2021-05-31 12:29:06 +0200

    BaseModel + Nuggets installed

---
+++ TecH3TheSmashBros.API/Models/BaseModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+//using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace TecH3TheSmashBros.API.Models
+{
+    public class BaseModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [JsonIgnore]
+        public DateTime CreateAt { get; set; }
+
+        [JsonIgnore]
+        public DateTime? UpdatedAt { get; set; }
+
+        [JsonIgnore]
+        public DateTime? DeletedAt { get; set; }
+    }
+}
