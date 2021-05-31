2c7fe802f6b2d996fc0f2c39aa7b36b78a10e5db
BerkSKP <60089989+BerkSKP@users.noreply.github.com>
2021-05-31 13:07:49 +0200

    mandages kode

---
+++ TecH3TheSmashBros.API/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace TecH3TheSmashBros.API.Models
+{
+    public class Category : BaseModel
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public string CategoryTitle { get; set; }
+    }
+}
