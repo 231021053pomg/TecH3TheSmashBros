6635bea95f81ddaf629b759450968ae31fb22bcb
LionheartIV <54432930+LionheartIV@users.noreply.github.com>
2021-05-31 13:07:52 +0200

    User repo started

--- TecH3TheSmashBros.API/Models/Users.cs
+++ TecH3TheSmashBros.API/Models/Users.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TecH3TheSmashBros.API.Models
 {
-    public class Users
+    public class Users : BaseModel
     {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(64, ErrorMessage = "Maximum 32 Chars")]
+
+        public string Email { get; set; }
+        [Required]
+        [StringLength(32, ErrorMessage = "Maximum 32 Chars")]
+
+        public string Password { get; set; }
+
+        //[ForeignKey("")]
+        public string UserType { get; set; }
     }
 }
