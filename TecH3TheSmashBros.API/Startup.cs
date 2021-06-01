91d748d87fef6bbba08a75bcd54483b2fd57eced
BerkSKP <60089989+BerkSKP@users.noreply.github.com>
2021-06-01 10:59:31 +0200

    DATABASECONTEXT

--- TecH3TheSmashBros.API/Startup.cs
+++ TecH3TheSmashBros.API/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TecH3TheSmashBros.API.Database;
 
 namespace TecH3TheSmashBros.API
 {
@@ -26,7 +28,9 @@ namespace TecH3TheSmashBros.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<TecH3TheSmashBrosDbContext>(
+               options => options.UseSqlServer(Configuration.GetConnectionString("MyConnection"))
+               );
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
