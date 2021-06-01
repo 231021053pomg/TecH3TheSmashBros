e2fbf359509f018c1cd58b9cf86c18d905a11d37
Jack Conradsen <jackconradsen@gmail.com>
2021-06-01 13:54:43 +0200

    Product repository

---
+++ TecH3TheSmashBros.API/Repositories/IProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TecH3TheSmashBros.API.Models;
+
+namespace TecH3TheSmashBros.API.Repositories
+{
+    interface IProductRepository
+    {
+        Task<List<User>> GetAllProducts();
+
+        Task<List<User>> GetProductsByCategory(Category category);
+
+        Task<User> GetProductById(int id);
+
+        Task<User> CreateProduct(Product product);
+
+        Task<User> UpdateProduct(int id, Product product);
+
+        Task<User> DeleteProduct(int id);
+
+    }
+}
---
+++ TecH3TheSmashBros.Tests/Repositories/ProductRepositryTest.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using TecH3TheSmashBros.API.Repositories;
+using TecH3TheSmashBros.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace TecH3TheSmashBros.Tests
+{
+    public class ProductRepositryTest
+    {
+        [Fact]
+        public void GetAllProducts()
+        {
+            private readonly DbContextOptions<TecH3TheSmashBrosDbContext> _options;
+            private readonly TecH3TheSmashBrosDbContext _context;
+        }
+    }
+}
