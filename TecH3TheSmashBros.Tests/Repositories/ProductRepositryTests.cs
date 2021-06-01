5a2f0e6f5898fc12c3adca602e73dd692ca35d61
Jack Conradsen <jackconradsen@gmail.com>
2021-06-01 14:05:45 +0200

    product additions

--- TecH3TheSmashBros.Tests/Repositories/ProductRepositryTests.cs
+++ TecH3TheSmashBros.Tests/Repositories/ProductRepositryTests.cs
@@ -4,16 +4,20 @@ using TecH3TheSmashBros.API.Repositories;
 using TecH3TheSmashBros.API.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using TecH3TheSmashBros.API.Database;
 
 namespace TecH3TheSmashBros.Tests
 {
-    public class ProductRepositryTest
+    public class ProductRepositryTests
     {
+
+        private readonly DbContextOptions<TecH3TheSmashBrosDbContext> _options;
+        private readonly TecH3TheSmashBrosDbContext _context;
+
         [Fact]
         public void GetAllProducts()
         {
-            private readonly DbContextOptions<TecH3TheSmashBrosDbContext> _options;
-            private readonly TecH3TheSmashBrosDbContext _context;
+        
         }
     }
 }
