9c5f7b6095510f40fb1633ed453e1d8a00bd34c3
BerkSKP <60089989+BerkSKP@users.noreply.github.com>
2021-06-07 13:02:45 +0200

    Start på OrderController

---
+++ TecH3TheSmashBros.API/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TecH3TheSmashBros.API.Services;
+
+
+namespace TecH3TheSmashBros.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+    }
+}
