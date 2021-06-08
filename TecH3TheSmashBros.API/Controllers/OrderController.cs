cda5ccca750024e523a4fbe1335ea15013b80ef0
BerkSKP <60089989+BerkSKP@users.noreply.github.com>
2021-06-08 12:36:31 +0200

    OrderController

--- TecH3TheSmashBros.API/Controllers/OrderController.cs
+++ TecH3TheSmashBros.API/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TecH3TheSmashBros.API.Models;
 using TecH3TheSmashBros.API.Services;
 
 
@@ -17,5 +18,104 @@ namespace TecH3TheSmashBros.API.Controllers
         {
             _orderService = orderService;
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var order = await _orderService.GetAllOrder();
+                if (order == null)
+                {
+                    return Problem("Din idiot");
+                }
+                if (order.Count == 0)
+                {
+                    return NoContent();
+                }
+                return Ok(order);
+
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            try
+            {
+                var order = await _orderService.GetOrderById(id);
+                if (order == null)
+                {
+                    return NotFound();
+                }
+                return Ok(order);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(Order order, List<OrderDetail> orderDetails)
+        {
+            try
+            {
+                if (order == null)
+                {
+                    return BadRequest("Author Fail.....");
+                }
+                var neworder = await _orderService.Create(order, orderDetails);
+
+                return Ok(neworder);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] Order order, List<OrderDetail> orderDetails)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    return BadRequest("CANNOT UPDATE");
+                }
+                var updateorder = await _orderService.Update(id, order,orderDetails);
+                if (updateorder == null)
+                {
+                    return NotFound("Could not found id");
+                }
+                return Ok(updateorder);
+
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    return BadRequest("CANNOT DELETE");
+                }
+                var Deleteorder = await _orderService.Delete(id);
+                return Ok(Deleteorder);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
     }
 }
---
+++ TecH3TheSmashBros.Tests/Controller/OrderControllerTest.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Xunit;
+using System.Threading.Tasks;
+using TecH3TheSmashBros.API.Database;
+using Microsoft.EntityFrameworkCore;
+using TecH3TheSmashBros.API.Models;
+using TecH3TheSmashBros.API.Repositories;
+using System.Collections.Generic;
+using TecH3TheSmashBros.API.Controllers;
+using TecH3TheSmashBros.API.Services;
+using Moq;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace TecH3TheSmashBros.Tests.Controller
+{
+    public class OrderControllerTest
+    {
+        private readonly OrderController _sut;
+        private readonly Mock<IOrderService> _orderServiceMock = new();
+
+        public OrderControllerTest()
+        {
+            _sut = new OrderController(_orderServiceMock.Object);
+        }
+        [Fact]
+        public async Task GetAll_ShouldReturn200_WhenDataExists()
+        {
+            // Arange
+            List<Order> orders = new List<Order>();
+
+            orders.Add(new Order());
+            orders.Add(new Order());
+
+            _orderServiceMock
+                .Setup(x => x.GetAllOrder())
+                .ReturnsAsync(orders);
+
+            // Act
+            var result = await _sut.GetAll();
+
+            // Assert
+            var StatsusCodeResuslt = (IStatusCodeActionResult)result;
+            Assert.Equal(200, StatsusCodeResuslt.StatusCode);
+        }
+        [Fact]
+        public async Task GetAll_ShouldReturn204_WhenDataIsEmpty()
+        {
+            // Arange
+            List<Order> orders = new List<Order>();
+
+
+            _orderServiceMock
+                .Setup(x => x.GetAllOrder())
+                .ReturnsAsync(orders);
+
+            // Act
+            var result = await _sut.GetAll();
+
+            // Assert
+            var StatsusCodeResuslt = (IStatusCodeActionResult)result;
+            Assert.Equal(204, StatsusCodeResuslt.StatusCode);
+        }
+        [Fact]
+        public async Task GetAll_ShouldReturn500_WhenDataExists()
+        {
+            // Arange
+            _orderServiceMock
+                .Setup(x => x.GetAllOrder())
+                .ReturnsAsync(() => null);
+
+            // Act
+            var result = await _sut.GetAll();
+
+            // Assert
+            var StatsusCodeResuslt = (IStatusCodeActionResult)result;
+            Assert.Equal(500, StatsusCodeResuslt.StatusCode);
+        }
+        [Fact]
+        public async Task Create_ShouldReturnStatus200_WhenOrderSubmitIsOk()
+        {
+            // Arrange
+            Order order = new Order
+            {
+                Id = 2,
+                UserId = 2,
+                Date = new DateTime(2009, 07, 22),
+                CreatedAt = new DateTime(2009, 07, 22)
+            };
+            List<OrderDetail> orderDetails = new List<OrderDetail>();
+            orderDetails.Add(new OrderDetail
+            {
+                Price = 2,
+                Amount = 5,
+                Id = 1,
+                CreatedAt = new DateTime(2009, 07, 22),
+                OrderId = 1
+            });
+            _orderServiceMock
+                .Setup(s => s.Create(It.IsAny<Order>(), It.IsAny<List<OrderDetail>>()))
+                .ReturnsAsync(order);
+
+            // Act
+            var response = await _sut.CreateOrder(order,orderDetails);
+
+            // Assert
+            // specifikt se på det ObjectResult der kommer fra _controller
+            var responseStatusCode = (IStatusCodeActionResult)response;
+            Assert.Equal(200, responseStatusCode.StatusCode);
+        }
+
+
+        [Fact]
+        public async Task GetOrderById_ShouldReturnOrder_WhenOrderExists()
+        {
+            // Arrange
+            int id = 1;
+            int userid = 1;
+            DateTime date = new DateTime(2011, 08, 30);
+            DateTime createdat = new DateTime(2011, 09, 30);
+
+            Order order = new Order
+            {
+                Id = id,
+                UserId = userid,
+                Date = date,
+                CreatedAt = createdat
+
+            };
+
+            _orderServiceMock
+                .Setup(s => s.GetOrderById(It.IsAny<int>()))
+                .ReturnsAsync(order);
+
+            // Act
+            var response = await _sut.Get(id);
+
+            // Assert
+            Assert.NotNull(response);
+        }
+    }
+}
