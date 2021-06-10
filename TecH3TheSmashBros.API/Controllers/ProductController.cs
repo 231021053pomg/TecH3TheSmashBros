f77818214f8a6e7ea058a0665f5642f658acc5d2
Jack Conradsen <jackconradsen@gmail.com>
2021-06-10 14:07:28 +0200

    vrag

---
+++ TecH3TheSmashBros.API/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TecH3TheSmashBros.API.Services;
+
+namespace TecH3TheSmashBros.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var products = await _productService.GetAllProducts();
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpGet("{categoryId}")]
+        public async Task<IActionResult> GetAllByCategory([FromRoute] int categoryId)
+        {
+            try
+            {
+                var products = await _productService.GetAllProductsByCategory(categoryId);
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            try
+            {
+                var product = await _productService.GetProductById(id);
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
