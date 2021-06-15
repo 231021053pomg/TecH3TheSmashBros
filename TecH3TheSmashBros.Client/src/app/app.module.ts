1c8bc994950e1c540a3cda53391bb82078a12ae8
BerkSKP <60089989+BerkSKP@users.noreply.github.com>
2021-06-15 14:03:05 +0200

    tirsdags arbejde

--- TecH3TheSmashBros.Client/src/app/app.module.ts
+++ TecH3TheSmashBros.Client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
