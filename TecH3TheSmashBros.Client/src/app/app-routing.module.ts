0dcec4df2479eddcc79e0792edb11b30dc895592
Jack Conradsen <jackconradsen@gmail.com>
2021-06-22 12:19:32 +0200

    product changes

--- TecH3TheSmashBros.Client/src/app/app-routing.module.ts
+++ TecH3TheSmashBros.Client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailComponent } from './product-page/product-detail/product-detail.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'products/:id',component:ProductDetailComponent},
+  {path: 'products',component:ProductPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
