b242dc8bc02d6964245375b15405c4c29eae026d
LionheartIV <54432930+LionheartIV@users.noreply.github.com>
2021-06-24 08:49:23 +0200

    Virker

--- TecH3TheSmashBros.Client/src/app/app-routing.module.ts
+++ TecH3TheSmashBros.Client/src/app/app-routing.module.ts
@@ -3,10 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailComponent } from './product-page/product-detail/product-detail.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { OrderComponent } from './order/order.component';
+import { LoginComponent } from './login/login.component';
+
 
 const routes: Routes = [
   // { path: '', redirectTo: '/CheckOut', component: CheckoutComponent},
-  { path: 'CheckOut', component:CheckoutComponent}
+  { path: 'CheckOut', component:CheckoutComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component:HomepageComponent},
+  { path: 'products',component:ProductPageComponent},
+  { path: 'Orders', component:OrderComponent},
+  { path: 'products/category',component:ProductPageComponent},
+  { path: 'products/category/:category_id',component:ProductPageComponent},
+  { path: 'products/:id',component:ProductDetailComponent},
+  { path: 'login', component:LoginComponent}
 
 ];
 
